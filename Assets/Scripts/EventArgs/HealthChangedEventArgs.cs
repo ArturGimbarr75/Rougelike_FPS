cdc1fdc2af10f2366cc66e93e4e9f01b89cd911b
ArturGimbarr75 <arturasik.r@gmail.com>
2024-01-17 18:40:41 +0200

    Add health

---
+++ Assets/Scripts/EventArgs/HealthChangedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿public class HealthChangedEventArgs
+{
+	public float Delta { get; private set; }
+	public float CurrentHealth => _health.Current;
+	public float MaxHealth => _health.Max;
+	public float Percent => _health.Percent;
+
+	private Health _health;
+
+	public HealthChangedEventArgs(float delta, Health health)
+	{
+		Delta = delta;
+		_health = health;
+	}
+}
