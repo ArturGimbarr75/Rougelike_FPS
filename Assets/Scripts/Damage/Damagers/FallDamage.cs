24eea725c3ca42a6c2027c2eccade4ecea9f348b
ArturGimbarr75 <arturasik.r@gmail.com>
2024-01-17 18:53:22 +0200

    Add fall damage

---
+++ Assets/Scripts/Damage/Damagers/FallDamage.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class FallDamage : MonoBehaviour
+{
+    [SerializeField, Min(0)] private float _damagePerVelocity = 10f;
+    [SerializeField, Min(0)] private float _minVelocity = 10f;
+
+    [SerializeField] private CharacterControllerGravity _gravity;
+    [SerializeField] private GeneralDamageProvider _damageProvider;
+
+    private void OnEnable()
+    {
+		_gravity.Land += OnLand;
+	}
+
+    private void OnDisable()
+    {
+		_gravity.Land -= OnLand;
+	}
+
+	private void OnLand(object sender, LandEventArgs args)
+    {
+		if (args.Velocity < _minVelocity)
+			return;
+
+        float damageValue = (args.Velocity - _minVelocity) * _damagePerVelocity;
+        Damage damage = new ()
+        {
+            Type = DamageType.Fall,
+            Value = damageValue
+        };
+        _damageProvider.ApplyDamage(damage);
+	}
+}
