316b78a2c9e476c602f5ac3a348fd4f2c53d0a4c
ArturGimbarr75 <arturasik.r@gmail.com>
2024-01-21 03:21:09 +0200

    Add level loading system

---
+++ Assets/Scripts/GameSystems/SceneSwitcher/SceneNames.cs
@@ -0,0 +1,4 @@
+﻿public static partial class SceneNames
+{
+	public const string MAIN_MENU = "MainMenu"; // TODO: Create later
+}
---
+++ Assets/Scripts/GameSystems/SceneSwitcher/TestSceneSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.SceneManagement;
+
+public static partial class SceneSwitcher
+{
+	public static void LoadMovementTestScene()
+	{
+		LoadScene(SceneNames.TEST_MOVEMENT);
+	}
+
+	public static void LoadPauseTestScene()
+	{
+		LoadScene(SceneNames.TEST_PAUSE);
+	}
+
+	public static void LoadHealthDamageTestScene()
+	{
+		LoadScene(SceneNames.TEST_HEALTH_DAMAGE);
+	}
+
+	public static void LoadGameOverTestScene()
+	{
+		LoadScene(SceneNames.TEST_GAME_OVER);
+	}
+}
