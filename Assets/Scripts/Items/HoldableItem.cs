845e58ab3e29b0f396b84c374008d202763dfb0a
ArturGimbarr75 <arturasik.r@gmail.com>
2024-03-03 20:12:53 +0200

    Add holldable item

---
+++ Assets/Scripts/Items/HoldableItem.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public abstract class HoldableItem : MonoBehaviour
+{
+    [field:SerializeField] public bool Droppable { get; private set; } = true;
+    [field:SerializeField] public Vector3 HoldLocalPosition { get; private set; }
+    [field:SerializeField] public Quaternion HoldLocalRotation { get; private set; }
+
+    [field:Space]
+    [field:SerializeField] public Sprite Icon { get; private set; }
+
+    public abstract void OnPickup();
+    public abstract void OnDrop();
+    public abstract void OnShow();
+    public abstract void OnHide();
+}
